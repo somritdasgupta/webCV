543cdd147097334b6eeeaaa8732c2af19cad1ebf
Somrit Dasgupta <somritdasgupta7@gmail.com>
2024-08-04 00:06:59 +0530

    Hydration Issues SSR

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({
       )}
     >
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="icon" href="/og.png" type="image/png" />
         <SandpackCSS />
@@ -97,7 +98,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer />
-          <Analytics />
+          <Analytics debug={false} mode={'production'} />
           <SpeedInsights />
         </main>
       </body>
