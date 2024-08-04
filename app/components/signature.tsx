3eeea312d5609fb84b1e2a01cf51be528c2f2c23
Somrit Dasgupta <somritdasgupta7@gmail.com>
2024-08-04 12:17:59 +0530

    UI Fixes

--- app/components/signature.tsx
+++ app/components/signature.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Signature: React.FC = () => {
   return (
-    <div className="font-cursive text-xl mt-4 text-gray-600 mt-4 italic flex flex-col items-start">
+    <div className="font-cursive text-xl mt-6 text-gray-600 mt-4 italic flex flex-col items-start">
       <span>Peace out,</span>
       <span>Somrit Dasgupta</span>
     </div>
